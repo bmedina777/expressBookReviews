4b752d6f9a945f014b7b3177a1772c2f1038bc0e
Deux777 <99618623+Deux777@users.noreply.github.com>
2024-09-22 16:59:17 -0600

    05-pipes
    Continuacion de Pipes

--- Angular/05-pipesApp/src/app/app.module.ts
+++ Angular/05-pipesApp/src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+//configuracion del locale de la app
+import  localeESHN  from '@angular/common/locales/es-HN';
+import  localeFRCA  from '@angular/common/locales/fr-CA';
+import  {registerLocaleData}  from '@angular/common';
 
+registerLocaleData(localeESHN);
+registerLocaleData(localeFRCA);
 
 @NgModule({
   declarations: [
@@ -18,7 +24,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    { provide: LOCALE_ID, useValue: 'es-HN' } // cambiar el locale según la necesidad
   ],
   bootstrap: [AppComponent]
 })
